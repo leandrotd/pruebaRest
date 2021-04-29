69ca41a4b09618420d02b433901bef298ca1b0df
leandrotd <leandrotd@users.noreply.github.com>
2021-04-29 17:18:03 +0200

    Inicio de la aplicación

---
+++ pruebaRest/src/main/java/es/leandro/PruebaRestApplication.java
@@ -0,0 +1,13 @@
+package es.leandro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaRestApplication.class, args);
+	}
+
+}
