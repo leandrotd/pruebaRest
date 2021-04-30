ebd1a57aa5ae22f2ffb35394aa4289efe9472f12
leandrotd <leandrotd@users.noreply.github.com>
2021-04-30 10:24:45 +0200

    Agregada la anotación CrossOrigin en localhost:4200

--- pruebaRest/src/main/java/es/leandro/controlador/ClienteRestController.java
+++ pruebaRest/src/main/java/es/leandro/controlador/ClienteRestController.java
@@ -3,6 +3,7 @@ package es.leandro.controlador;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import es.leandro.modelo.entity.Cliente;
 import es.leandro.modelo.sevices.IClienteServices;
 
+@CrossOrigin(origins= {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
